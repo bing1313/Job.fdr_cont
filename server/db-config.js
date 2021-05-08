71e3d4f5f094b8ea4d615b960cc25833818b3738
bing1313 <79605037+bing1313@users.noreply.github.com>
2021-05-08 08:38:57 -0400

    Update db-config.js

--- server/db-config.js
+++ server/db-config.js
@@ -1,9 +1,1 @@
-// Private. Will not be included in submission
-// Replace <Your Pennkey> and <Your Password>
-module.exports = {
-    host: "fling.seas.upenn.edu",
-    user: "bing13",
-    password: "SuckAtCoding131n9!",
-    database: "bing13"
-  };
-  
+
