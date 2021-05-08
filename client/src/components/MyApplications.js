59abf2ef91fba9963feca2760337fdc3a71b9fd0
bing1313 <79605037+bing1313@users.noreply.github.com>
2021-05-08 08:21:44 -0400

    industry-search

--- client/src/components/MyApplications.js
+++ client/src/components/MyApplications.js
@@ -11,14 +11,22 @@ export default class MyApplications extends React.Component {
 		this.state = {
 			selectedDecade: "",
 			selectedGenre: "",
+			selectedIndustries: [],
 			decades: [],
 			genres: [],
-			movies: []
+			movies: [],
+			originalSuggestions: [],
+			suggestions: [],
 		};
 
 		this.submitDecadeGenre = this.submitDecadeGenre.bind(this);
 		this.handleDecadeChange = this.handleDecadeChange.bind(this);
 		this.handleGenreChange = this.handleGenreChange.bind(this);
+		this.handleIndustryInputChange = this.handleIndustryInputChange.bind(this);
+	 	this.deleteFromArray = this.deleteFromArray.bind(this);
+		this.decadesFilter = this.deleteFilter.bind(this);
+		//this.selectIndustry = this.selectIndustry.bind(this);
+		//this.showSuggestions = this.showSuggestions.bind(this);
 	};
 
 	/* ---- Q3a (Best Movies) ---- */
@@ -63,14 +71,25 @@ export default class MyApplications extends React.Component {
 		  }).then(genresList => {
 			if (!genresList) return;
 	  
+			let suggestionsList = [];
+			for (var i = 0; i < genresList.length; i++){
+				var obj = genresList[i];
+				console.log("genres object " + i + " " + obj.name);
+				suggestionsList.push(obj.name);
+			}
+
+			console.log("this is the suggestions list " + suggestionsList);
 			// Map each genre in this.state.genres to an HTML option:
 			const genresDivs = genresList.map((genreObj, i) =>
 			  <option className="genresOption" key={i} value={genreObj.name}>{genreObj.name}</option>
+			  
 			);
 	  
 			// Set the state of the genre list to the value returned by the HTTP response from the server.
 			this.setState({
-			  genres: genresDivs
+			  genres: genresDivs,
+			  originalSuggestions: suggestionsList 
+			  
 			});
 		  }, err => {
 			// Print the error if there is one.
@@ -91,6 +110,120 @@ export default class MyApplications extends React.Component {
 			selectedGenre: e.target.value
 		});
 	};
+
+	/**
+	 * 
+	 * @param {*} e 
+	 * when the input changes, filter the original suggestion for words that include 
+	 * the input. 
+	 */
+	handleIndustryInputChange(e) {
+	 const input = e.target.value;
+	 console.log("input "+ input);
+	 
+	 let new_suggestions = [];
+	 if (input.length > 0){
+		const regex = new RegExp(`^${input}`, `i`);
+		new_suggestions = this.state.originalSuggestions.sort().filter(item => regex.test(item));
+
+		console.log("new suggestion list" + new_suggestions);
+		//new_suggestions = ["hello", "hi"];
+	 }
+	 console.log("final state new_suggestions before set state" + new_suggestions);
+	
+	 this.setState({
+		 suggestions: new_suggestions
+		 
+	 });
+
+	 console.log("suggestions state handle Change" + this.state.suggestions);
+
+	}
+	
+	/**
+	 * display the updated selectedIndustries list
+	 * @returns 
+	 */
+	showSuggestions () {
+		const { suggestions } = this.state.suggestions; //change this later
+		// if (suggestions.length === 0){
+		// 	return null;
+		// }
+
+		return (
+			<ul>
+				{this.state.suggestions.map((item) => 
+					<button onClick={this.selectIndustry.bind(this, item)}> {item} </button>)
+					}
+			</ul>
+		);
+	} 
+
+	/**
+	 * 
+	 * @param {*} item 
+	 * @returns When industry item is clicked, add it to list of selected industries and display it
+	 * If the selected option already exists, then do nothing. Called when an industry button is selected
+	 * to add to filters list
+	 */
+	selectIndustry(item){
+		
+		console.log("clicked item " + item);
+       
+		let pickedIndustries= this.state.selectedIndustries;
+		if (!pickedIndustries.includes(item)){
+			pickedIndustries.push(item);
+		}
+		
+		this.setState({
+			selectedIndustries: pickedIndustries
+		})
+
+		console.log("selected industries list function " + this.state.selectedIndustries);
+	}
+
+	/**
+	 * 
+	 * @param {*} category number 1,2 ,3.... corresponds to the appropraite list of filters that is
+	 * being updated
+	 * 1: industry
+	 * 2: sector
+	 * 3: salary
+	
+	 * @param {*} item the acutal value that matched in seach and deleted if it exists in the array
+	 */
+	deleteFilter(item){
+		console.log("delete selection industries: " + this.state.selectedIndustries); 
+		console.log("delete filter item" + item);
+	   let industryFilters = this.state.selectedIndustries;
+	
+	//    if (industryFilters.includes(item)){
+	// 		console.log("conatins");
+		   this.deleteFromArray(industryFilters, item);
+	//    } else {
+	// 	   console.log("doesn't contain");
+	//    }
+		  console.log("after return industry filters " + industryFilters);
+		  this.setState({
+			  selectedIndustries: industryFilters
+		  })
+	 	  this.showSuggestions();
+
+	}
+	
+	deleteFromArray(filtersList, item) {
+		console.log("delete from array item: " + item);
+		console.log("delete from array INITIAL array: "+ filtersList);
+
+		
+		for (var i=0; i < filtersList.length; i++){
+			if (filtersList[i] === item){
+				filtersList.splice(i, 1);
+			}
+		}
+
+		console.log("delete from array FINAL array: "+ filtersList);
+	}
 
 	/* ---- Q3b (Best Movies) ---- */
 	submitDecadeGenre() {
@@ -131,7 +264,7 @@ export default class MyApplications extends React.Component {
 		return (
 			<div className="MyApplications">
 				
-				<PageNavbar active="applications" />
+				<PageNavbar active="applications"/>
 
 				<div className="container applications-container">
 					<div className="jumbotron">
@@ -140,11 +273,34 @@ export default class MyApplications extends React.Component {
 							<select value={this.state.selectedDecade} onChange={this.handleDecadeChange} className="dropdown" id="decadesDropdown">
 								{this.state.decades}
 							</select>
-							<select value={this.state.selectedGenre} onChange={this.handleGenreChange} className="dropdown" id="genresDropdown">
+							<select type="text" value={this.state.selectedGenre} onChange={this.handleGenreChange} className="dropdown" id="genresDropdown">
 								{this.state.genres}
 							</select>
 							<button className="submit-btn" id="submitBtn" onClick={this.submitDecadeGenre}>Submit</button>
 						</div>
+
+						<div className="h5">Industry</div>
+						<div className="industy-dropdown-container">
+							<input type="text" name="industry-input"
+								onChange={this.handleIndustryInputChange}/>
+								{this.showSuggestions()}
+							
+						</div>
+						<div className="selected-industries">
+							{this.state.selectedIndustries.map((item) => {
+								return (
+									
+									<div className="industry-filter">{item}
+									<button type="button" onClick={this.deleteFilter.bind(this,item)}>Delete</button>
+									</div>
+									
+								)
+							})}
+						</div>
+
+						<button className="filter-submit-btn" id="filter-submitBtn" onClick={this.submitDecadeGenre}>Submit</button>
+
+
 					</div>
 					<div className="jumbotron">
 						<div className="movies-container">
